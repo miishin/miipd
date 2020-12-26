56ceffa627b17ec770b5689b99961d922ba523ec
miishin <miishing@gmail.com>
2020-12-25 21:49:06 -0500

    added board + tiles, removed x.txt

--- assets/scripts/character_select.gd
+++ assets/scripts/character_select.gd
@@ -6,7 +6,7 @@ var input = Vector2.ZERO
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	var selection = $Panel/Selection
+	var selection = $Selection
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	velocity = input.normalized()
