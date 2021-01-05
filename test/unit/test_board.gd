9fa3de2777a02de46776bf15ee287866fce8a1e3
miishin <miishing@gmail.com>
2021-01-05 16:34:07 -0500

    refactor so that each test only has one assertion

--- test/unit/test_board.gd
+++ test/unit/test_board.gd
@@ -11,16 +11,20 @@ func test_initialize_tiles():
 		assert_eq(len(_board.tiles[i]), 7)
 
 func test_neighbors():
-	var neighbors = _board.tiles[0][0].get_neighbors() 
-	assert_has(neighbors, _board.tiles[1][0])
-	assert_has(neighbors, _board.tiles[0][1])
+	var neighbors = _board.tiles[0][0].get_neighbors()
+	var result = false
+	result = neighbors.has(_board.tiles[1][0])
+	result = neighbors.has(_board.tiles[0][1])
+	assert_true(result)
 
 # Neighbors	
 func test_find_accessible_tiles_dist1():
 	var neighbors = _board.tiles[3][3].get_neighbors()
 	var accessible = _board.find_accessible_tiles(_board.tiles[3][3], 1)
+	var result
 	for tile in neighbors:
-		assert_has(accessible, tile)
+		result = accessible.has(tile)
+	assert_true(result)
 	
 func test_find_accessible_tiles_dist2():
 	var accessible = _board.find_accessible_tiles(_board.tiles[3][3], 2)
@@ -34,14 +38,18 @@ func test_find_accessible_tiles_dist2():
 	reachable_tiles.append(_board.tiles[4][2])
 	reachable_tiles.append(_board.tiles[4][4])
 	reachable_tiles.append(_board.tiles[5][3])
+	var result = false
 	for reachable_tile in reachable_tiles:
-		assert_has(accessible, reachable_tile) 
+		result = accessible.has(reachable_tile)
+	assert_true(result)
 
 func test_pathfinder():
 	var path = [_board.tiles[0][0], _board.tiles[1][0], _board.tiles[2][0], _board.tiles[3][0], _board.tiles[4][0]]
 	var found_path = _board._pathfinder(_board.tiles[0][0], _board.tiles[4][0], 4)
+	var result = false
 	for tile in path:
-		assert_has(found_path, tile)
+		result = found_path.has(tile)
+	assert_true(result)
 
 func test_pathfinder_with_obstacle():
 	_board.tiles[1][0].passable = false
