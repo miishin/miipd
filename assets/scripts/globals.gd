bf856d0d07364f2a22d012ce48b314f9ee8b4f04
miishin <miishing@gmail.com>
2021-01-10 12:41:36 -0500

    Character Selection Screen (#24)
    * project.godot
    
    * basic work on character selection screen
    
    * transition to game
    
    * clicking same tile again removes it from q
    
    * check if tile is selected
    
    * main -> character select and back works, passing through characters to game should work
    
    * proper transition from select to game
    
    * refactor to connect signals in script not manually to one function:
    
    * disconnected old signals
    
    * buttons now toggle on or off based on selection
    
    * delete accidentally created directory background/, make scene point to correct file in backgrounds/

--- assets/scripts/globals.gd
+++ assets/scripts/globals.gd
@@ -1,6 +1,25 @@
 extends Node
 
+var player_characters
+
 var playing     : bool
 var tween_count : int
 
 var yielded_animations = []
+
+var ability_map = {
+	"bash": make_ability(2, Ability.RangeAngle.FULL, Vector2(1, 1), Ability.AoEType.CIRCULAR, 0, Ability.AbilityTarget.ENEMY, Ability.Debuff.NONE, 90),
+}
+
+static func make_ability(dmg, angle, ability_range, aoe_type, aoe, target, debuff, acc) -> Ability:
+	var ability = Ability.new()
+	ability.damage = dmg
+	ability.range_angle = angle
+	ability.ability_range = ability_range
+	ability.aoe_type = aoe_type
+	ability.aoe = aoe
+	ability.target = target
+	ability.debuff = debuff
+	ability.accuracy = acc
+	ability.description = "Does " + str(dmg) + " damage"
+	return ability
