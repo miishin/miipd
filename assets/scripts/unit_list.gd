f3838bf93e19d33500b0b9b9df6bdee658610322
miishin <miishing@gmail.com>
2021-02-26 15:53:53 -0500

    unit turn queue implementation

--- assets/scripts/unit_list.gd
+++ assets/scripts/unit_list.gd
@@ -4,7 +4,7 @@ class_name UnitList
 const SPACING = 0
 
 var rng = RandomNumberGenerator.new()
-var units : Array = []
+var all_units : Array = []
 var total_speed : int
 var queue : Array = []
 
@@ -12,18 +12,18 @@ func _ready():
 	update()
 
 func _init(units : Array):
-	units = units
-	for unit in units:
+	all_units = units
+	for unit in all_units:
 		total_speed += unit.spd
 	generate_next_n_units(5)
 	
 
 func add_unit(unit : Unit):
-	units.append(unit)
+	all_units.append(unit)
 	total_speed += unit.spd
 	
 func remove_unit(unit : Unit):
-	units.erase(unit)
+	all_units.erase(unit)
 	queue.erase(unit)
 	total_speed -= unit.spd
 
@@ -33,16 +33,15 @@ func generate_next_n_units(n : int):
 	
 func generate_next_unit() -> Unit:
 	rng.randomize()
-	var next = rng.randf_range(0, total_speed)
+	var next = rng.randf_range(1, total_speed)
 	return _find_unit(next)
 	
 func _find_unit(speed : int) -> Unit:
-	var counter = 0
-	for unit in units:
-		if speed < unit.spd + counter:
-			return unit
+	var counter = 1
+	for unit in all_units:
 		counter += unit.spd
-	print("uh oh")
+		if speed < counter:
+			return unit
 	return null
 
 func next_unit() -> Unit:
