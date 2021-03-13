883314915c7e5ea08d0db442e80511362ca5c45b
miishin <miishing@gmail.com>
2021-03-13 14:12:28 -0500

    Turnqueuefix (#69)
    * new keybindings
    
    * tutorial
    
    * fixed turn queue in game size
    
    * fixed issue with isaac's code

--- assets/scripts/unit_list.gd
+++ assets/scripts/unit_list.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name UnitList
 
-const SPACING = 0
+const SPACING = 50
 
 var units = []
 
@@ -12,8 +12,9 @@ func add_unit(unit : Unit):
 	if len(units) == 0:
 		unit.position.x = 0
 	else:
-		unit.position.x = units[-1].position.x + units[-1].get_size().x + SPACING
+		unit.position.x = units[-1].position.x + units[-1].get_size().x * 4 + SPACING
 	units.append(unit)
+	update()
 	add_child(unit)
 
 func update():
@@ -21,7 +22,8 @@ func update():
 	for unit in units:
 		assert(unit is Unit)
 		unit.position.x = start
-		start += unit.get_size().x + SPACING
+		start += unit.get_size().x * 4 + SPACING
+		unit.scale = Vector2(5, 5)
 		
 func delete_unit(unit : Unit):
 	units.erase(unit)
